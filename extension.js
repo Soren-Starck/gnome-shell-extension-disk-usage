bd421178de1b9e53c257f490c064726c20600ba0
0CT0PUCE <115957989+0CT0PUCE-LOG@users.noreply.github.com>
2023-03-30 00:27:11 +0200

    added top comment

--- extension.js
+++ extension.js
@@ -1,22 +1,18 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-/* exported init */
+/**********************************************************************
+*
+*   Extension created by :
+*   ___   _____ _______ ___  _____  _    _  _____ ______ 
+*  / _ \ / ____|__   __/ _ \|  __ \| |  | |/ ____|  ____|
+* | | | | |       | | | | | | |__) | |  | | |    | |__   
+* | | | | |       | | | | | |  ___/| |  | | |    |  __|  
+* | |_| | |____   | | | |_| | |    | |__| | |____| |____ 
+*  \___/ \_____|  |_|  \___/|_|     \____/ \_____|______|
+*
+* DiskUsage is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as
+* published by the Free Software Foundation.
+*
+**********************************************************************/ 
 
 const GETTEXT_DOMAIN = 'my-indicator-extension';
 
